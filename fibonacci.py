32a622135876ce26622438531410036455539735
DaliborLysak <lysak.dalibor@gmail.com>
2022-07-30 22:05:52 +0200

    fibonacci

---
+++ fibonacci.py
@@ -0,0 +1,21 @@
+from pickle import TRUE
+
+num = int(input())
+print("output:")
+
+def fibonacci(n):
+    list = []
+    go(n,list)
+    for x in list[len(list)-(n+1)::]:
+        print(x)
+
+def go(n, list):
+    if n <= 1:
+        list.append(n)
+        return n
+    else:
+        number = go(n - 2, list)  + go(n - 1, list)
+        list.append(number)
+        return number          
+
+fibonacci(num)
